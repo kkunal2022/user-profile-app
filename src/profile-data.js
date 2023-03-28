b5d1ccfc5c79a11a7595cb3d8a71668da7d726eb
Kumar Kunal <84966950+kkunal2022@users.noreply.github.com>
2023-03-28 10:34:00 +0300

    Updating Mine Profile Link
    Updating Mine Profile Link

--- src/profile-data.js
+++ src/profile-data.js
@@ -1,23 +1,9 @@
 export const profiles = [
   {
-    img: "https://i.ibb.co/7W7rV8v/profile2.png",
+    img: "https://i.ibb.co/XJFMctP/Mine-Photo1.jpg",
     name: "Kumar Kunal",
-    job: "Technical Lead Developer",
+    job: "Technical Lead, System Architect & Lead Software Engineer",
     company: "Ahli United Bank",
     link: "http://linkedin.com/in/kumar-kunal-3538b123",
-  },
-  {
-    img: "https://i.ibb.co/gvwfFJK/profile1.png",
-    name: "Adaora Nwodo",
-    job: "Cloud Engineer",
-    company: "Microsoft",
-    link: "https://twitter.com/AdoraNwodo",
-  },
-  {
-    img: "https://i.ibb.co/tKn71dn/profile3.png",
-    name: "Fisayo Tinuke",
-    job: "Mobile Developer",
-    company: "ZinoTrust",
-    link: "#",
   },
 ];
